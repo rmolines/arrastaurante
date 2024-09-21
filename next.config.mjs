555dfa12da687ca89f0c6317ca5d487fe602743a
Rafael Molines Ismael <rafaelmismael@gmail.com>
2024-09-21 01:03:20 -0300

    fdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "places.googleapis.com",
-				port: "",
-				pathname: "/v1/**",
-			},
-		],
 		domains: ["places.googleapis.com"],
 	},
 	typescript: {
